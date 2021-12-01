727f46505af9617b573844a12e8d11d8d49901b5
Cristo378 <cristovaohenrique157@gmail.com>
2021-12-01 12:14:39 +0000

    consulta

--- components/formulario.js
+++ components/formulario.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, {useState} from 'react'
+import { Text, View, TextInput, Button } from 'react-native'
 
 export default function Formulario () {
+        const [cep, onChangeCep] = useState('')
+        const [cidade, onChangeCidade] = useState('')
+
+        const handleConsultar = () => {
+            const url = 'https://viacep.com.br/ws/' + cep + '/json/'
+            fetch(url, {
+                method: 'GET',
+                 headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json' 
+                 }
+            }).then(function(response){
+                response.json() .then(function(data){
+                    onChangeCidade(data['localidade'])
+                })     
+            })
+        }
     return(
         <View>
-                <Text>Novo componente</Text>
+            <Text>Digete um CEP (somente números):</Text>
+            <TextInput value={cep} onChangeText={onChangeCep} />
+            <Button title="Consultar" onPress={handleConsultar} />
+            <Text>{cidade}</Text>
         </View>
     )
 }
